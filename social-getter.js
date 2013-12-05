3d74f65ed4aecaf96dedd8b0a2fd40bb8efb2c87
bellum <bellumm@gmail.com>
2013-12-05 02:29:18 +0200

    added pinned-to-top field into post template

--- social-getter.js
+++ social-getter.js
@@ -69,7 +69,9 @@
 
 					posts:
 				 		'{{#each posts}}' +
-							'<div class="post {{_id}}" id="{{_id}}" data-visible="{{accepted}}" style="width: {{../column_width}}px; left: 0px; top: 0px">' +
+							'<div class="post {{_id}}" id="{{_id}}"' +
+							'data-visible="{{accepted}}" data-pinned-to-top="{{pinned-to-top}}"' +
+							'style="width: {{../column_width}}px; left: 0px; top: 0px">' +
 								'<div class="post-holder">' +
 									'<div class="post-content">' +
 										'{{#unless image}}<div class="post-icon">' +
